d19ff00f6f5b768b2d2a9f122be7b57ab3dadf33
Alexander Biryukov <bisiro@mail.ru>
2025-04-18 23:51:41 +0300

    first attempt

---
+++ home_work_1/tree.c
@@ -0,0 +1,44 @@
+/*
+ * hello_world
+ * 
+ * Copyright 2025 Professional <Professional@USER-PC>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+char f0[]="   *   \n";
+char f1[]="  ***  \n";
+char f2[]=" ***** \n";
+char f3[]="*******\n";
+char f4[]=" HHOHH \n";
+char f5[]=" ZZZZZ \n";
+
+int main(void)
+{
+	printf(f0);
+	printf(f1);
+	printf(f2);
+	printf(f3);
+	printf(f4);
+	printf(f5);
+	return 0;
+}
+
