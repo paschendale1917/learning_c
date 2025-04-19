00f41551cbf9857e1ea5b14a0f2e1383963b0acf
paschendale1917 <62398274+paschendale1917@users.noreply.github.com>
2025-04-19 08:19:47 +0300

    Update tree.c

--- home_work_1/tree.c
+++ home_work_1/tree.c
@@ -3,20 +3,17 @@
 #include <stdio.h>
 
 char f0[][9]={"   *   \n",
-					  "  ***  \n",
-					 " ***** \n",
-					"*******\n",
-					" HHOHH \n",
-					" ZZZZZ "
-					};
+	      "  ***  \n",
+	      " ***** \n",
+	      "*******\n",
+	      " HHOHH \n",
+	      " ZZZZZ "
+	    };
 
-
-int main(void)
-{
-	for(int i=0;i<6;i++){
-		printf(*(f0+i));
-	}
-
-	return 0;
+int main(void){
+ for(int i=0;i<6;i++){
+  printf(*(f0+i));
+ }
+ return 0;
 }
 
